73463ed63efd424ea2bfb96d5afcb54559dfec80
Nicolas <ns.descamps@gmail.com>
2024-02-11 12:06:26 +0100

    fix: add h1

--- element/style.go
+++ element/style.go
@@ -2,12 +2,6 @@ package element
 
 import "syscall/js"
 
-var appStyle = js.Global().Get("document").Call("createElement", "style")
-
-func init() {
-	appStyle.Set("type", "text/css")
-}
-
 type Style struct {
 	el      js.Value
 	elStyle js.Value
@@ -20,35 +14,35 @@ func NewStyle(el js.Value) *Style {
 	}
 }
 
-func (s *Style) SetBackgroundColor(color string) {
+func (s Style) SetBackgroundColor(color string) {
 	s.elStyle.Set("backgroundColor", color)
 }
 
-func (s *Style) SetColor(color string) {
+func (s Style) SetColor(color string) {
 	s.elStyle.Set("color", color)
 }
 
-func (s *Style) SetWidth(width string) {
+func (s Style) SetWidth(width string) {
 	s.elStyle.Set("width", width)
 }
 
-func (s *Style) SetHeight(height string) {
+func (s Style) SetHeight(height string) {
 	s.elStyle.Set("height", height)
 }
 
-func (s *Style) H_PaMa0() {
+func (s Style) H_PaMa0() {
 	s.elStyle.Set("padding", 0)
 	s.elStyle.Set("margin", 0)
 }
 
-func (s *Style) SetDisplay(display string) {
+func (s Style) SetDisplay(display string) {
 	s.elStyle.Set("display", display)
 }
 
-func (s *Style) SetOverflow(overflow string) {
+func (s Style) SetOverflow(overflow string) {
 	s.elStyle.Set("overflow", overflow)
 }
 
-func (s *Style) SetClassName(className string) {
+func (s Style) SetClassName(className string) {
 	s.el.Set("className", className)
 }
--- node/H.go
+++ node/H.go
@@ -8,12 +8,27 @@ import (
 )
 
 type (
+	H1 struct {
+		// ref
+		ref   *js.Value
+		Style *element.Style
+	}
+
 	H2 struct {
 		// ref
 		ref   *js.Value
 		Style *element.Style
 	}
 )
+
+func NewH1(text string) *H1 {
+	el := core.Document.Call("createElement", "h1")
+	el.Set("innerText", text)
+	return &H1{
+		ref:   &el,
+		Style: element.NewStyle(el),
+	}
+}
 
 func NewH2(text string) *H2 {
 	el := core.Document.Call("createElement", "h2")
@@ -22,6 +37,16 @@ func NewH2(text string) *H2 {
 		ref:   &el,
 		Style: element.NewStyle(el),
 	}
+}
+
+func (h *H1) Render() element.Element {
+	return h
+}
+func (h *H1) Value() *js.Value {
+	return h.ref
+}
+func (h *H1) SetInnerText(text string) {
+	h.ref.Set("innerText", text)
 }
 
 func (h *H2) Render() element.Element {
